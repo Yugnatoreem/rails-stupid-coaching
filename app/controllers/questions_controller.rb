bc61897b16f8f6f9e06e0c9991f781cf5c909bb4
Tanguy Lafeuille <lafeuilletanguy@gmail.com>
2023-05-22 12:37:40 +0200

    Exercice Terminé + opt

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,11 +3,12 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    @result = params['result']
+    @question = params['question']
 
-    if @result == 'I am going to work'
+    if @question == 'I am going to work'
       @answer = 'Great!'
-    elsif @result.end_with?('?')
+      # take_screenshot
+    elsif @question.end_with?('?')
       @answer = 'Silly question, get dressed and go to work!'
     else
       @answer = "I don't care, get dressed and go to work!"
